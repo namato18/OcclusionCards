bfb7257bfc3bb3820f089f4e3f29189cc272bb15
michael j Swallower <nickamato608@gmail.com>
2023-09-02 09:37:07 -0400

    added loop to save images

--- imgTesting.R
+++ imgTesting.R
@@ -2,24 +2,20 @@ library(tesseract)
 library(magick)
 
 eng = tesseract('eng')
-text = ocr("~/Downloads/Screenshot 2023-08-30 at 10.24.27 PM.png")
+text = ocr("C:/Users/xbox/Pictures/demo1.png")
 
-df = ocr_data("~/Downloads/Screenshot 2023-08-30 at 10.24.27 PM.png")
+df = ocr_data("C:/Users/xbox/Pictures/demo1.png")
 
 df = df[df$confidence > 90,]
 
 words = df$word[which(df$confidence >= 90)]
 x = paste(words, collapse = " ")
 
-img = magick::image_read("~/Downloads/Screenshot 2023-08-30 at 10.24.27 PM.png")
+img = magick::image_read("C:/Users/xbox/Pictures/demo1.png")
 
-image_crop(img, "1000x1124+610+0", repage = FALSE)
-
-
-image_annotate(img, "CONFIDENTIAL", color = "red", boxcolor = "pink",
-               degrees = 0, location = "200x200+72+65")
 
 img = image_draw(img)
+image_write(img, path = paste0("test_images/test.png"), format = "png")
 for(i in 1:nrow(df)){
   coord = df$bbox[i]
   coord = strsplit(coord,split = ",")[[1]]
@@ -28,7 +24,7 @@ for(i in 1:nrow(df)){
   coord3 = as.numeric(coord[3])
   coord4 = as.numeric(coord[4])
   rect(coord1,coord2,coord3,coord4, col = "red", lwd = 5)
+  image_write(img, path = paste0("test_images/test",i,".png"), format = "png")
+  
 }
 
-
-rect(72,66,270,145, col = "red", lwd = 5)
