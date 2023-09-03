9f60ef64e04ef94c71a6ddf09e9533bcc8c45380
michael j Swallower <nickamato608@gmail.com>
2023-09-03 14:46:41 -0400

    app layout updates

--- ImageOcclusionApp/app.R
+++ ImageOcclusionApp/app.R
@@ -3,29 +3,42 @@ library(magick)
 library(tesseract)
 # Define UI for application
 ui <- fluidPage(
-  fileInput("filein","Input Your Images Here", multiple = TRUE),
-  actionButton("generate","Generate Cards"),
-  imageOutput("imgOut")
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("filein","Input Your Images Here", multiple = TRUE),
+      actionButton("generate","Generate Cards")
+      
+    ),
+    mainPanel(
+      imageOutput("imgOut"),
+      actionButton("nextImage","Remove One Rectangle (bottom up")
+    )
+    
+  )
+
   
 )
 
 # Define server logic
 server <- function(input, output) {
   
+  image.counter = reactiveValues()
+  image.counter$val = 0
+  
   observeEvent(input$generate, {
     
     f = input$filein
-    
     for(i in 1:nrow(f)){
-      
-      
+      # Grab filepath
       path = f$datapath[i]
       
+      # Grab words from picture
       eng = tesseract('eng')
       df = ocr_data(path)
       df = df[df$confidence > 90,]
       words = df$word[which(df$confidence >= 90)]
       
+      # Create images with boxes over words
       img = magick::image_read(path)
       img = image_draw(img)
       image_write(img, path = paste0("../test_images/test",i,".png"), format = "png")
@@ -37,17 +50,32 @@ server <- function(input, output) {
         coord3 = as.numeric(coord[3])
         coord4 = as.numeric(coord[4])
         rect(coord1,coord2,coord3,coord4, col = "red", lwd = 5)
-        image_write(img, path = paste0("../test_images/test",j,i,".png"), format = "png")
+        image_write(img, path = paste0("../test_images/test",i,"_",j,".png"), format = "png")
         
       }
     }
-    
-    output$imgOut = renderImage(    list(src = "../test_images/test1.png",
-                                         contentType = 'image/png',
-                                         width = 1000,
-                                         height = 750,
-                                         alt = "This is alternate text"),
-                                    deleteFile = FALSE)
+    list.of.images = sort(list.files(path = "../test_images"))
+    assign("list.of.images",list.of.images,.GlobalEnv)
+    output$imgOut = renderImage(list(src = paste0("../test_images/",list.of.images[length(list.of.images)]),
+                                     contentType = 'image/png',
+                                     width = 1000,
+                                     height = 750,
+                                     alt = "This is alternate text"),
+                                deleteFile = FALSE)
+  })
+  
+  observeEvent(input$nextImage, {
+    if(image.counter$val < (length(list.of.images)-1)){
+      print(list.of.images[length(list.of.images)-image.counter$val])
+      output$imgOut = renderImage(    list(src = paste0("../test_images/",list.of.images[length(list.of.images)-image.counter$val]),
+                                           contentType = 'image/png',
+                                           width = 1000,
+                                           height = 750,
+                                           alt = "This is alternate text"),
+                                      deleteFile = FALSE)
+      image.counter$val = image.counter$val + 1
+    }
+
   })
   
   
